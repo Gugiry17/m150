71a7d4528afbb62271eef537ea75b2aabc555836
Micha Baumann <micha.baumann@gmx.ch>
2023-09-01 12:40:25 +0200

    feat: :lipstick: changed some ui things for the bitcoin page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -72,13 +72,12 @@ const config = {
         items: [
           {
             type: "docSidebar",
-            sidebarId: "tutorialSidebar",
+            sidebarId: "bitcoinSidebar",
             position: "left",
-            label: "Tutorial",
+            label: "Bitcoin",
           },
-          { to: "/blog", label: "Blog", position: "left" },
           {
-            href: "https://github.com/facebook/docusaurus",
+            href: "https://www.tbzwiki.ch/index.php?title=Modul_150",
             label: "GitHub",
             position: "right",
           },
@@ -86,48 +85,7 @@ const config = {
       },
       footer: {
         style: "dark",
-        links: [
-          {
-            title: "Docs",
-            items: [
-              {
-                label: "Tutorial",
-                to: "/docs/intro",
-              },
-            ],
-          },
-          {
-            title: "Community",
-            items: [
-              {
-                label: "Stack Overflow",
-                href: "https://stackoverflow.com/questions/tagged/docusaurus",
-              },
-              {
-                label: "Discord",
-                href: "https://discordapp.com/invite/docusaurus",
-              },
-              {
-                label: "Twitter",
-                href: "https://twitter.com/docusaurus",
-              },
-            ],
-          },
-          {
-            title: "More",
-            items: [
-              {
-                label: "Blog",
-                to: "/blog",
-              },
-              {
-                label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        links: [],
       },
       prism: {
         theme: lightCodeTheme,
